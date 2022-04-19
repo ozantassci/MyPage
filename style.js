b59bc71d2ef853a287149e23882c6cb6fd5501de
Ozan TAŞCI <102819318+ozantassci@users.noreply.github.com>
2022-04-19 22:18:48 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,29 @@
+$(function () {
+    $(window).on('scroll', function () {
+        if ( $(window).scrollTop() > 10 ) {
+            $('.navbar').addClass('active');
+        } else {
+            $('.navbar').removeClass('active');
+        }
+    });
+});
+
+
+
+
+
+// BACK TO TOP DENEME
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
